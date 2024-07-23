0a9ff31aa7407ec28f47a8cd4fa4975241c103b3
AugustoCSantos <augusto.santos131012@gmail.com>
2024-07-23 10:47:51 -0300

    ...

--- scripts/player.gd
+++ scripts/player.gd
@@ -14,16 +14,12 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_FORCE
 		is_jump = true
 	elif is_on_floor():
 		is_jump = false
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction != 0:
 		velocity.x = direction * SPEED
@@ -35,9 +31,7 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		animation.play("Idle")
-
 	move_and_slide()
-
 func _on_hurtbox_body_entered(body):
 	if body.is_in_group("enemies"):
 		queue_free()
