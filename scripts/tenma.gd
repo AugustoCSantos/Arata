ad3fd6e54a6cd4678dd7aee64aed04dc857f646b
AugustoCSantos <augusto.santos131012@gmail.com>
2024-07-23 01:27:39 -0300

    fase 3

---
+++ scripts/tenma.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+const SPEED = 3000.0
+const JUMP_VELOCITY = -400.0
+
+@onready var anima = $AnimatedSprite2D
+@onready var wall_detector = $wall_detector
+
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var direction := -1
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+	if wall_detector.is_colliding():
+		direction *= -1
+		wall_detector.scale.x *= -1
+		
+	if direction == -1:
+		anima.flip_h = true
+	else:
+		anima.flip_h = false
+	
+	
+	velocity.x = direction * SPEED* delta
+
+	move_and_slide()
